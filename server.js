2730870e934205fc8c76dea4748c7ef5a4f52bd3
frchase <69663529+frchase@users.noreply.github.com>
2023-05-09 11:45:27 -0400

    test

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ import minimist from "minimist";
 import express, { json, urlencoded } from "express";
 import { rps, rpsls } from './lib/rpsls.js'
 
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+const app = express();
+
+app.use(json());
+app.user(urlencoded({ extended: true}));
+
